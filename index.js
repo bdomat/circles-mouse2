25f3094b03c6b87e58ebab5fc1814ea6ab45a671
bdomat <bertrand.domat@gmail.com>
2023-01-12 23:01:33 +0100

    initialisation du projet

---
+++ index.js
@@ -0,0 +1,9 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+window.addEventListener("mousemove", (e) => {
+  console.log(mousemove.e);
+});
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
